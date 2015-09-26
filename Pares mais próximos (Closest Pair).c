5e1f6cfb99ad4eed3c4f8a8a1603a44c9d89c937
gulourenco <gustavolourenco7@hotmail.com>
2015-09-26 11:26:31 -0300

    Update Pares mais próximos (Closest Pair).c

--- Pares mais próximos (Closest Pair).c
+++ Pares mais próximos (Closest Pair).c
@@ -1,7 +1,7 @@
 #include<stdio.h>                               
 #include<stdlib.h>
 
-void read_numbers(int v[], int max)                  //Função para ler as entradas dos números e armazená-la no vetor
+void read_numbers(int v[], int max)                  //FunÃ§Ã£o para ler as entradas dos nÃºmeros e armazenÃ¡-la no vetor
 {
   int i = 0;
   int num;
@@ -13,7 +13,7 @@ void read_numbers(int v[], int max)                  //Função para ler as entrad
   }
 }
 
-void insertion(int vet[], int n)                    //Função para ordenar os elementos do vetor
+void insertion(int vet[], int n)                    //FunÃ§Ã£o para ordenar os elementos do vetor
 {
     int i, j, temp;
         for (i = 1; i < n; i++)
@@ -36,16 +36,16 @@ main()
 {
 	int *vet;                                 //Declaro um vetor de inteiros
 	int i=1;
-	int n, x1, x2, dif1, dif2;                //Declaroção algumas variáveis que serão utilizados ao longo do programa
-	printf("Digite o numero de inteiros a serem lidos: ");   //Entra com o número n que define o total de valores a serem lidos
+	int n, x1, x2, dif1, dif2;                //DeclaroÃ§Ã£o algumas variÃ¡veis que serÃ£o utilizados ao longo do programa
+	printf("Digite o numero de inteiros a serem lidos: ");   //Entra com o nÃºmero n que define o total de valores a serem lidos
 	scanf("%d", &n);
-	vet=(int*)malloc(sizeof(int)*n);                         //Alocação de memória para um vetor com n elementos
+	vet=(int*)malloc(sizeof(int)*n);                         //AlocaÃ§Ã£o de memÃ³ria para um vetor com n elementos
     read_numbers(vet, n);                                    //Leitura das entradas e armazenamento no vetor
-	insertion(vet, n);                                       //Ordenação do vetor
+	insertion(vet, n);                                       //OrdenaÃ§Ã£o do vetor
 	dif1=vet[1]-vet[0];
 	x1=vet[0];
 	x2=vet[1];
-	while(i<n-2)                          //Percorre o vetor buscando o par de números com a menor diferença e armazena-os nas variáveis x1 e x2
+	while(i<n-2)                          //Percorre o vetor buscando o par de nÃºmeros com a menor diferenÃ§a e armazena-os nas variÃ¡veis x1 e x2
     {
     	dif2=vet[i+1]-vet[i];
 		if(dif2<dif1)
@@ -56,9 +56,9 @@ main()
 		}
 		i++;
     }
-    printf("O par de numero com menor diferenca eh:\n\n");     //Imprime o par de número com a menor diferença
+    printf("O par de numero com menor diferenca eh:\n\n");     //Imprime o par de nÃºmero com a menor diferenÃ§a
     printf("%d e %d", x1, x2);
-    free(vet);                                                 //Desalocaçãode memória utilizada
+    free(vet);                                                 //DesalocaÃ§Ã£o de memÃ³ria utilizada
 }
 
 
